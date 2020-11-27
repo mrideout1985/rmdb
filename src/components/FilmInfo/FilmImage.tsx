e72d0aba5ecdb4711ea9ed60802ed4a2bd3a9a17
mrideout1985 <mrideout1985@gmail.com>
2020-11-27 02:06:46 +0000

    refactored code

--- src/components/FilmInfo/FilmImage.tsx
+++ src/components/FilmInfo/FilmImage.tsx
@@ -3,31 +3,33 @@ import { Film } from "../../hooks/search/interface";
 import styles from "./FilmImage.module.scss";
 
 interface Props {
-	element: React.ReactNode;
+	film: Film | undefined;
 }
 
-const FilmImage: React.FC<Props> = ({ element }) => {
-	let film = element as Film;
+const FilmImage: React.FC<Props> = ({ film }) => {
+	const handleFilmNames = () => {
+		if (film) {
+			if (film?.media_type === "tv") {
+				return <h3>{film.name}</h3>;
+			} else {
+				return <h3>{film.title}</h3>;
+			}
+		}
+	};
 
 	return (
 		<>
 			<div className={styles.container}>
 				<img
 					src={
-						film?.poster_path !== undefined
-							? `https://image.tmdb.org/t/p/original${film?.poster_path}`
+						film
+							? `https://image.tmdb.org/t/p/original${film.poster_path}`
 							: `https://place-hold.it/300x500/aaa/WHITE&text=NO-IMAGE-AVAILABLE&fontsize=20`
 					}
 					alt=""
 				/>
 
-				<div className={styles.filminfo}>
-					{film?.media_type === "tv" ? (
-						<h3>{film.name}</h3>
-					) : (
-						<h3>{film.title}</h3>
-					)}
-				</div>
+				<div className={styles.filminfo}>{handleFilmNames()}</div>
 			</div>
 		</>
 	);
