4bc020e6f4fb611b60e69f1eccebfd3b9588ac86
mrideout1985 <mrideout1985@gmail.com>
2020-11-27 01:51:28 +0000

    obsolete file - will delete.

--- src/components/Cast/CastImage.tsx
+++ src/components/Cast/CastImage.tsx
@@ -1,36 +1,7 @@
 import React from "react";
-import { Link } from "react-router-dom";
-import { TrendingPeopleResults } from "../../hooks/search/interface";
-import styles from "./CastImage.module.scss";
 
-interface PersonImageProps {
-	element: React.ReactNode;
-}
-
-const CastImage: React.FC<PersonImageProps> = ({ element }) => {
-	let el = element as TrendingPeopleResults;
-
-	return (
-		<>
-			{el?.profile_path === undefined ? null : (
-				<Link
-					to={`/person-page/${el?.id}`}
-					className={styles["container"]}
-				>
-					<div
-						style={{
-							height: "5rem",
-							width: "5rem",
-							borderRadius: "50%",
-							backgroundSize: "cover",
-							backgroundPosition: "center",
-							backgroundImage: `url(https://image.tmdb.org/t/p/original${el?.profile_path})`,
-						}}
-					/>
-				</Link>
-			)}
-		</>
-	);
+const CastImage: React.FC = () => {
+	return null;
 };
 
 export { CastImage };
